d0b89b1570591806ce69c57761eef9dd5a4fe351
Justin Ramel <justin.ramel@gmail.com>
2011-02-05 21:55:58 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitflowTest::Application.config.secret_token = 'efb473690855c4716a1147af1f6c21ab4cc6f61a9c69a4419cb8a8d4b4e4f13dbc0e5d93c68503bdad9bf95dafd78898ebde83f8fab346ecebe8333a6d005ac5'
